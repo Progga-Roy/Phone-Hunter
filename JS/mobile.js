cb28808ed0eb51e038c3bb6d889d72d6c69ea0a2
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-16 07:25:27 -0700

    phone display by search name

---
+++ JS/mobile.js
@@ -0,0 +1,35 @@
+const loadPhone = async(searchText)=>{
+const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+const res = await fetch(url)
+const data = await res.json()
+displayLoadPhone(data.data)
+}
+
+
+const displayLoadPhone =phones=>{
+    const mobileContainer = document.getElementById('mobile-container');
+         mobileContainer.innerText ='';
+    phones.forEach(phone => {
+        // console.log(phone);
+        const createDiv = document.createElement('div')
+        createDiv.classList.add('col')
+        createDiv.innerHTML =`
+        <div class="card p-4 shadow">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">${phone.brand}</p>
+        </div>
+      </div>
+        `
+        mobileContainer.appendChild(createDiv);
+    });
+
+}
+
+document.getElementById('search-btn').addEventListener('click',function(){
+const searchField = document.getElementById('search-field').value;
+loadPhone(searchField)
+
+})
+loadPhone();
